da21e6da218dd6dc9b60b331b3b57672f31ca8c5
jcarloshg <carlosj12336@gmail.com>
2025-01-08 00:04:07 -0600

    add decorador

--- src/structures/Decorator/HTML/index.ts
+++ src/structures/Decorator/HTML/index.ts
@@ -5,9 +5,36 @@ import { ElementDecoratorHTML } from "./ElementDecoratorHTML";
 
 const dataToRender = "Hello, World!";
 
+const concreteElement_01 = new ConcreteElement();
+const decoratorDiv_01 = new ElementDecoratorDiv(concreteElement_01);
+const decoratorDiv_02 = new ElementDecoratorDiv(decoratorDiv_01);
+decoratorDiv_02.render(dataToRender);
+
+// ============================================================
+// >>> output
+// <div>
+//   <div>
+//     Hello, World!
+//   </div>
+// </div>
+// ============================================================
+
 const concreteElement = new ConcreteElement();
 const decoratorDiv01 = new ElementDecoratorDiv(concreteElement);
 const decoratorDiv02 = new ElementDecoratorDiv(decoratorDiv01);
-const decoratorBody = new ElementDecoratorBody(decoratorDiv02)
+const decoratorBody = new ElementDecoratorBody(decoratorDiv02);
 const elementDecoratorHTML = new ElementDecoratorHTML(decoratorBody);
 elementDecoratorHTML.render(dataToRender);
+
+// ============================================================
+// >>> output
+// <html>
+//      <body>
+//          <div>
+//              <div>
+//                  Hello, World!
+//              </div>
+//          </div>
+//      </body>
+// </html>
+// ============================================================
